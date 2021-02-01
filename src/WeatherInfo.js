eed1651fb4f3dbf2a92550d0c1b452cc1af8bc71
chloekim917 <chloekim917@gmail.com>
2021-02-01 12:48:44 -0500

    api env setup

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
+const weatherApiToken = process.env.OPEN_WEATHER_KEY
+
 class WeatherInfo extends React.Component {
     render(){
+        
         return (
             <div>
                 <h1>Simple Weather!!!!</h1>
