7efdea328d689ac86e3c40585faaf41bbc3cbc81
chloekim917 <chloekim917@gmail.com>
2021-02-01 12:36:29 -0500

    initial setup

---
+++ src/WeatherInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class WeatherInfo extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Simple Weather!!!!</h1>
+            </div>
+        )
+    }
+}
+
+export default WeatherInfo;
